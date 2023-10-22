c03bc49c45c83b4ede9aac2e94496a2bd54c6fde
amir-zarchini <amzarchini@gmail.com>
2023-10-22 11:46:58 +0330

    add checkout model

---
+++ src/main/java/com/example/library/model/Checkout.java
@@ -0,0 +1,35 @@
+package com.example.library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "checkout")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Checkout {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Date checkoutDate;
+
+    private Date expirationDate;
+}
